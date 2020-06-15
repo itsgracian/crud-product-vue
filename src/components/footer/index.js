1c0a07f26bd769a9cd0b8e8ee1089b5affa72c92
itsgracian <gracian2020@gmail.com>
2020-06-16 01:00:47 +0200

    footer component added

---
+++ src/components/footer/index.js
@@ -0,0 +1,6 @@
+Vue.component('footer-component', {
+    template: `<div class="app-container">
+    <div class="mt-5">All right reserved ${new Date().getFullYear()}</div>
+    <div class="bold">@ gratien</div>
+    </div>`
+});
